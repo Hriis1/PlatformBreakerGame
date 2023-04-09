66502f6ac44424fb37034f9bd97088b26ad7ae82
hrishd <hrishd@abv.bg>
2023-04-09 22:02:42 +0300

    rectangle class added

---
+++ Engine/RectF.cpp
@@ -0,0 +1,21 @@
+#include "RectF.h"
+
+RectF::RectF(float left_, float right_, float top_, float bot_)
+	: left(left_), right(right_), top(top_), bot(bot_)
+{
+}
+
+RectF::RectF(const Vec2& topLeft, const Vec2& botRight)
+	: RectF(topLeft.x, botRight.x, topLeft.y, botRight.y)
+{
+}
+
+RectF::RectF(const Vec2& topLeft, float width, float height)
+	: RectF(topLeft.x, topLeft.x + width, topLeft.y, topLeft.y + height)
+{
+}
+
+bool RectF::isOverlappingWith(const RectF& other) const
+{
+	return right >= other.left && left <= right && bot >=other.top && top <= other.bot;
+}
---
+++ Engine/RectF.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Vec2.h"
+class RectF
+{
+public:
+	RectF() = default;
+	RectF(float left_, float right_, float top_, float bot_);
+	RectF(const Vec2& topLeft, const Vec2& botRight);
+	RectF(const Vec2& topLeft, float width, float height);
+
+	bool isOverlappingWith(const RectF& other) const;
+public:
+	float left = 0.0f;
+	float right = 0.0f;
+	float top = 0.0f;
+	float bot = 0.0f;
+};
+
